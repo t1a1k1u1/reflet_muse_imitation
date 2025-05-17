379801cd0e72c1364513d8d205a000bab1d68353
t1a1k1u1 <koishi.t81@gmail.com>
2025-05-17 15:29:03 +0900

    correction of target position

--- scenes/play/container.gd
+++ scenes/play/container.gd
@@ -7,12 +7,14 @@ var target_positions
 
 
 func _ready() -> void:
+	var object_radius = GlobalConstants.OBJECT_SIZE
+	
 	spawn_positions = $SpawnPosition.get_children().map(func(e): return e.position)
 	
 	var targets_origin = $TargetPosition.get_children().map(func(e): return e.position)
 	target_positions = targets_origin \
-		+ targets_origin.map(func(e): return e * Vector2(-1, 1)) \
-		+ targets_origin.map(func(e): return e + Vector2(get_viewport_rect().size.x, 0))
+		+ targets_origin.map(func(e): return e * Vector2(-1, 1) + Vector2(object_radius, 0)) \
+		+ targets_origin.map(func(e): return e + Vector2(get_viewport_rect().size.x - object_radius, 0))
 
 
 func _on_spawn_timer_timeout() -> void:
