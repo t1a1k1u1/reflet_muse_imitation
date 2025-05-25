09c70274cf66f9bfa141f27c8afe3b649d851bba
t1a1k1u1 <koishi.t81@gmail.com>
2025-05-25 17:58:22 +0900

    draw the chain

--- scenes/object/chain_object.gd
+++ scenes/object/chain_object.gd
@@ -7,4 +7,11 @@ func initialize(spawn_pos: Vector2, target_pos: Vector2, times: Array) -> void:
 	for time in times:
 		var obj = normal_object.instantiate()
 		obj.initialize(spawn_pos, target_pos, time)
-		add_child(obj)
+		$ObjParent.add_child(obj)
+
+
+func _process(delta: float) -> void:
+	var objects = $ObjParent.get_children()
+	$Chain.clear_points()
+	for object in objects:
+		$Chain.add_point(object.position)
